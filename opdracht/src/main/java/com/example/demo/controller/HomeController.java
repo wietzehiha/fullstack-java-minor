230673e89fc28768364be13940eabfcbd554e362
wietzehiha <wietzehiha@gmail.com>
2020-09-22 19:37:09 +0200

    Opdracht 3
    oeps opdracht 2 overgeslagen, maar zit in deze en de vorige commit.

--- opdracht/src/main/java/com/example/demo/controller/HomeController.java
+++ opdracht/src/main/java/com/example/demo/controller/HomeController.java
@@ -12,18 +12,30 @@ public class HomeController {
     String reverse;
 
     @GetMapping("/begin")
-    public String createProjectForm(Model model) {
+    public String createForm(Model model) {
 
         model.addAttribute("form", new Form());
         return "home";
     }
 
-    @PostMapping("/result")
-    public String saveProjectSubmission(@ModelAttribute Form form, Model model) {
+    @PostMapping("/omdraaien")
+    public String omdraaienForm(@ModelAttribute Form form, Model model) {
 
         System.out.println("Input:" + form.getInputText());
 
-        reverse = stringController.reverse(form.getInputText());
+        reverse = "Woord omgedraaid: " + stringController.reverse(form.getInputText());
+
+        model.addAttribute("reverse", reverse);
+
+        return "home";
+    }
+
+    @PostMapping("/tellen")
+    public String countWordsForm(@ModelAttribute Form form, Model model) {
+
+        System.out.println("Input:" + form.getInputText());
+
+        reverse = "Aantal woorden: " + stringController.countWords(form.getInputText());
 
         model.addAttribute("reverse", reverse);
 
--- opdracht/src/main/java/com/example/demo/controller/stringController.java
+++ opdracht/src/main/java/com/example/demo/controller/stringController.java
@@ -28,4 +28,32 @@ public class stringController {
         return output;
     }
 
+    public static int countWords(String input) {
+        int wordCount = 0;
+
+        if(input == null) {
+            return wordCount;
+        }
+
+        //remove leading and trailing white spaces first
+        input = input.trim();
+
+        //if string contained all the spaces, word count is 0
+        if(input.equals("")) {
+            return wordCount;
+        }
+
+        wordCount = 1;
+
+        for(int i = 0; i < input.length(); i++){
+
+            //if character is a space, increase the word count
+            if( input.charAt(i) == ' ' ){
+                wordCount++;
+            }
+        }
+
+        return wordCount;
+    }
+
 }
